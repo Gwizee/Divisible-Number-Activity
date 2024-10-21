d21749e0856f15a44b5c222598bff22f6f7df64d
Gwizee <risingfire69@gmail.com>
2024-10-21 21:30:27 +0600

    Divisible Number Activity has completed

---
+++ main.py
@@ -0,0 +1,7 @@
+numerator = int(input("Enter a numerator : "))
+denominator = int(input("Enter a denominator : "))
+
+if numerator % denominator == 0 :
+    print(numerator , "is divisble by" , denominator)
+else :
+    print(numerator , "is not divisble by" , denominator)
